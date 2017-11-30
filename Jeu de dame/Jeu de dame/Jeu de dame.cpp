d394d50648a0734aeae9c56207337b572ed7a6ed
Antoine Molé <33541100+Antoine-Mole@users.noreply.github.com>
2017-11-30 10:51:52 +0100

    on remplit le plateau avec les pions

--- Jeu de dame/Jeu de dame/Jeu de dame.cpp
+++ Jeu de dame/Jeu de dame/Jeu de dame.cpp
@@ -2,10 +2,55 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+//creation plateau
+int tab[10][10];
+
+void Ecrire_tab()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << tab[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
 
 int main()
 {
-    return 0;
+	//initialisation du jeu
+//mettre toutes les cases vides
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			tab[i][j] = 0;
+	//mettre les pions blancs
+	for (int i = 0; i < 5; i++)
+	{
+		tab[0][2 * i] = 1;
+		tab[1][2 * i + 1] = 1;
+		tab[2][2 * i] = 1;
+		tab[3][2 * i + 1] = 1;
+	}
+	//mettre les pions noirs
+	for (int i = 0; i < 5; i++)
+	{
+		tab[6][2 * i] = 2;
+		tab[7][2 * i + 1] = 2;
+		tab[8][2 * i] = 2;
+		tab[9][2 * i + 1] = 2;
+	}
+
+	//on test
+	Ecrire_tab();
+
+	getchar();
+
+	return 0;
 }
 
